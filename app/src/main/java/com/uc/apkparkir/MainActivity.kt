ae577d0dcffd488efb39319c65823992d2b320c2
JasonEmanuel <jemanuel@student.ciputra.ac.id>
2022-06-06 15:38:36 +0700

    APK Parkir

--- app/src/main/java/com/uc/apkparkir/MainActivity.kt
+++ app/src/main/java/com/uc/apkparkir/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.uc.apkparkir
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.uc.apkparkir.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding=ActivityMainBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
